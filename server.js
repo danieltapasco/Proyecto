d039a2e943e4d679b4f41d7b9c9dec624649a715
Daniel Tapasco <daniel.tapasco@ucp.edu.co>
2022-11-19 14:02:58 -0500

    d

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-<html>
-    <head>
-        <title>Mi primer codigo JavaScript</title>
-    </head>
-    <body>
-        <h1>Mi primer código JavaScript</h1>
-        <script>
-            document.write("Hola Mundo");
-        </script>
-    </body>
-</html>
+console.log("Hello world!");
