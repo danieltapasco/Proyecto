46c4f5a1af3c2cf76c23dfd7ddfc73236d56f5d4
Daniel Tapasco <daniel.tapasco@ucp.edu.co>
2022-11-19 16:03:36 -0500

    ssa

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const server=http.createServer((req, res) => {
   res.sertHeader('Content-Type','text/plain');
   res.end('Hello patron');
 });
-server.listen(port,host, ()=> {
+server.listen(port, ()=> {
     console.log(`Server is running on:${port}`);
 });
