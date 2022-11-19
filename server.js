3a13d24e1adcb6777fa6d0e7d1d4ef80b20736f2
Daniel Tapasco <daniel.tapasco@ucp.edu.co>
2022-11-19 15:48:40 -0500

    sfd

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http= require('http');
 const port = 3001;
-const host = 'localhost';
 
 const server=http.createServer((req, res) => {
   res.statusCode= 200;
@@ -8,5 +7,5 @@ const server=http.createServer((req, res) => {
   res.end('Hello patron');
 });
 server.listen(port,host, ()=> {
-    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Server is running on:${port}`);
 });
