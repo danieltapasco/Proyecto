f60d681f1688fff6e947c67bb6573a1302053a95
Daniel Tapasco <daniel.tapasco@ucp.edu.co>
2022-11-19 13:57:18 -0500

    hola

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http= require('http');
-const port= 3001;
-
-const server=http.createServer((req, res) => {
-  res.statusCode= 200;
-  res.sertHeader('Content-Type','text/plain');
-  res.end('Hello patron');
-});
-server.listen(port, ()=> {
-  console.log(`Server running on port: ${port}`);
-});
+<html>
+    <head>
+        <title>Mi primer codigo JavaScript</title>
+    </head>
+    <body>
+        <h1>Mi primer código JavaScript</h1>
+        <script>
+            document.write("Hola Mundo");
+        </script>
+    </body>
+</html>
